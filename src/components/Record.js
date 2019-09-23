781339c257a43658f6c35f51b05deba22656c812
hatructu <hatructu@gmail.com>
2019-09-23 13:27:13 +0700

    chua record dc

--- src/components/Record.js
+++ src/components/Record.js
@@ -4,19 +4,79 @@ import {
     View, StyleSheet, Image, TouchableOpacity, TouchableHighlight,
 } from 'react-native'
 import AudioRecorderPlayer from 'react-native-audio-recorder-player'
-import { Stopwatch, Timer } from 'react-native-stopwatch-timer'
-
+const audioRecorderPlayer = new AudioRecorderPlayer()
 export default class Record extends Component {
     constructor(props) {
         super(props);
-        
-    }                                        
-    
+        this.state = {
+            recordSecs: null,
+            recordTimes: null,
+            playTime: null,
+            duration: null,
+        }
+    }
+    onStartRecord = async () => {
+        const result = await audioRecorderPlayer.startRecorder();
+        audioRecorderPlayer.addRecordBackListener((e) => {
+            this.setState({
+                recordSecs: e.current_position,
+                recordTime: audioRecorderPlayer.mmssss(
+                    Math.floor(e.current_position),
+                ),
+            });
+            return;
+        });
+        console.log(result);
+    }
+
+    onStopRecord = async () => {
+        const result = await audioRecorderPlayer.stopRecorder();
+        audioRecorderPlayer.removeRecordBackListener();
+        this.setState({
+            recordSecs: 0,
+        });
+        console.log(result);
+    }
+    onStartPlay = async () => {
+        console.log('onStartPlay');
+        const msg = await audioRecorderPlayer.startPlayer();
+        console.log(msg);
+        this.audioRecorderPlayer.addPlayBackListener((e) => {
+          if (e.current_position === e.duration) {
+            console.log('finished');
+            this.audioRecorderPlayer.stopPlayer();
+          }
+          this.setState({
+            currentPositionSec: e.current_position,
+            currentDurationSec: e.duration,
+            playTime: this.audioRecorderPlayer.mmssss(Math.floor(e.current_position)),
+            duration: this.audioRecorderPlayer.mmssss(Math.floor(e.duration)),
+          });
+          return;
+        });
+      };
     render() {
-       
+        
         return (
             <View style={{ flex: 1 }}>
-               
+                <TouchableOpacity
+                    onPress={()=>{this.onStartRecord()}}
+                >
+                    <Text>Start</Text>
+                </TouchableOpacity>
+
+                <TouchableOpacity
+                    onPress={()=>{this.onStopRecord()}}
+                >
+                    <Text>Stop</Text>
+                </TouchableOpacity>
+                
+                <TouchableOpacity
+                    onPress={()=>{this.onStartPlay()}}
+                >
+                    <Text>Play</Text>
+                </TouchableOpacity>
+                
             </View>
         )
     }
