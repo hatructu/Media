f41c9dbe4697ffee3aa0aa871d9267a89fce919c
hatructu <hatructu@gmail.com>
2019-09-23 17:50:02 +0700

    recorder

--- src/components/Record.js
+++ src/components/Record.js
@@ -3,87 +3,223 @@ import {
     Text,
     View, StyleSheet, Image, TouchableOpacity, TouchableHighlight,
 } from 'react-native'
-import AudioRecorderPlayer from 'react-native-audio-recorder-player'
-const audioRecorderPlayer = new AudioRecorderPlayer()
+import { AudioRecorder, AudioUtils } from 'react-native-audio';
+import Sound from 'react-native-sound'
+
 export default class Record extends Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            recordSecs: null,
-            recordTimes: null,
-            playTime: null,
-            duration: null,
-        }
-    }
-    onStartRecord = async () => {
-        const result = await audioRecorderPlayer.startRecorder();
-        audioRecorderPlayer.addRecordBackListener((e) => {
-            this.setState({
-                recordSecs: e.current_position,
-                recordTime: audioRecorderPlayer.mmssss(
-                    Math.floor(e.current_position),
-                ),
-            });
-            return;
-        });
-        console.log(result);
-    }
-
-    onStopRecord = async () => {
-        const result = await audioRecorderPlayer.stopRecorder();
-        audioRecorderPlayer.removeRecordBackListener();
-        this.setState({
-            recordSecs: 0,
-        });
-        console.log(result);
-    }
-    onStartPlay = async () => {
-        console.log('onStartPlay');
-        const msg = await audioRecorderPlayer.startPlayer();
-        console.log(msg);
-        this.audioRecorderPlayer.addPlayBackListener((e) => {
-          if (e.current_position === e.duration) {
-            console.log('finished');
-            this.audioRecorderPlayer.stopPlayer();
-          }
-          this.setState({
-            currentPositionSec: e.current_position,
-            currentDurationSec: e.duration,
-            playTime: this.audioRecorderPlayer.mmssss(Math.floor(e.current_position)),
-            duration: this.audioRecorderPlayer.mmssss(Math.floor(e.duration)),
-          });
-          return;
-        });
+    state = {
+        currentTime: 0.0,
+        recording: false,
+        paused: false,
+        stoppedRecording: false,
+        finished: false,
+        audioPath: AudioUtils.DocumentDirectoryPath + '/test.aac',
+        hasPermission: undefined,
       };
-    render() {
-        
+  
+      prepareRecordingPath(audioPath){
+        AudioRecorder.prepareRecordingAtPath(audioPath, {
+          SampleRate: 22050,
+          Channels: 1,
+          AudioQuality: "Low",
+          AudioEncoding: "aac",
+          AudioEncodingBitRate: 32000
+        });
+      }
+  
+      componentDidMount() {
+        AudioRecorder.requestAuthorization().then((isAuthorised) => {
+          this.setState({ hasPermission: isAuthorised });
+  
+          if (!isAuthorised) return;
+  
+          this.prepareRecordingPath(this.state.audioPath);
+  
+          AudioRecorder.onProgress = (data) => {
+            this.setState({currentTime: Math.floor(data.currentTime)});
+          };
+  
+          AudioRecorder.onFinished = (data) => {
+            // Android callback comes in the form of a promise instead.
+            if (Platform.OS === 'ios') {
+              this._finishRecording(data.status === "OK", data.audioFileURL, data.audioFileSize);
+            }
+          };
+        });
+      }
+  
+      _renderButton(title, onPress, active) {
+        var style = (active) ? styles.activeButtonText : styles.buttonText;
+  
         return (
-            <View style={{ flex: 1 }}>
-                <TouchableOpacity
-                    onPress={()=>{this.onStartRecord()}}
-                >
-                    <Text>Start</Text>
-                </TouchableOpacity>
+          <TouchableHighlight style={styles.button} onPress={onPress}>
+            <Text style={style}>
+              {title}
+            </Text>
+          </TouchableHighlight>
+        );
+      }
+  
+    //   _renderPauseButton(onPress, active) {
+    //     var style = (active) ? styles.activeButtonText : styles.buttonText;
+    //     var title = this.state.paused ? "RESUME" : "PAUSE";
+    //     return (
+    //       <TouchableHighlight style={styles.button} onPress={onPress}>
+    //         <Text style={style}>
+    //           {title}
+    //         </Text>
+    //       </TouchableHighlight>
+    //     );
+    //   }
+  
+    //   async _pause() {
+    //     if (!this.state.recording) {
+    //       console.warn('Can\'t pause, not recording!');
+    //       return;
+    //     }
+  
+    //     try {
+    //       const filePath = await AudioRecorder.pauseRecording();
+    //       this.setState({paused: true});
+    //     } catch (error) {
+    //       console.error(error);
+    //     }
+    //   }
+  
+    //   async _resume() {
+    //     if (!this.state.paused) {
+    //       console.warn('Can\'t resume, not paused!');
+    //       return;
+    //     }
+  
+    //     try {
+    //       await AudioRecorder.resumeRecording();
+    //       this.setState({paused: false});
+    //     } catch (error) {
+    //       console.error(error);
+    //     }
+    //   }
+  
+      async _stop() {
+        if (!this.state.recording) {
+          console.warn('Can\'t stop, not recording!');
+          return;
+        }
+  
+        this.setState({stoppedRecording: true, recording: false, paused: false});
+  
+        try {
+          const filePath = await AudioRecorder.stopRecording();
+  
+          if (Platform.OS === 'android') {
+            this._finishRecording(true, filePath);
+          }
+          return filePath;
+        } catch (error) {
+          console.error(error);
+        }
+      }
+  
+      async _play() {
+        if (this.state.recording) {
+          await this._stop();
+        }
 
-                <TouchableOpacity
-                    onPress={()=>{this.onStopRecord()}}
-                >
-                    <Text>Stop</Text>
-                </TouchableOpacity>
-                
-                <TouchableOpacity
-                    onPress={()=>{this.onStartPlay()}}
-                >
-                    <Text>Play</Text>
-                </TouchableOpacity>
-                
+        setTimeout(() => {
+          var sound = new Sound(this.state.audioPath, '', (error) => {
+            if (error) {
+              console.log('failed to load the sound', error);
+            }
+          });
+  
+          setTimeout(() => {
+            sound.play((success) => {
+              if (success) {
+                console.log('successfully finished playing');
+              } else {
+                console.log('playback failed due to audio decoding errors');
+              }
+            });
+          }, 100);
+        }, 100);
+      }
+  
+      async _record() {
+        if (this.state.recording) {
+          console.warn('Already recording!');
+          return;
+        }
+  
+        if (!this.state.hasPermission) {
+          console.warn('Can\'t record, no permission granted!');
+          return;
+        }
+  
+        if(this.state.stoppedRecording){
+          this.prepareRecordingPath(this.state.audioPath);
+        }
+  
+        this.setState({recording: true, paused: false});
+  
+        try {
+          const filePath = await AudioRecorder.startRecording();
+        } catch (error) {
+          console.error(error);
+        }
+      }
+  
+      _finishRecording(didSucceed, filePath, fileSize) {
+        this.setState({ finished: didSucceed });
+        console.log(`Finished recording of duration ${this.state.currentTime} seconds at path: ${filePath} and size of ${fileSize || 0} bytes`);
+      }
+  
+      render() {
+  
+        return (
+          <View style={styles.container}>
+            <View style={styles.controls}>
+              {this._renderButton("RECORD", () => {this._record()}, this.state.recording )}
+              {this._renderButton("PLAY", () => {this._play()} )}
+              {this._renderButton("STOP", () => {this._stop()} )}
+              {/* {this._renderPauseButton(() => {this.state.paused ? this._resume() : this._pause()})} */}
+              <Text style={styles.progressText}>{this.state.currentTime}s</Text>
             </View>
-        )
+          </View>
+        );
+      }
     }
-}
-const styles = StyleSheet.create({
-    container: {
+  
+    var styles = StyleSheet.create({
+      container: {
         flex: 1,
-        flexDirection: 'column',
-    },
-})
+        backgroundColor: "#2b608a",
+      },
+      controls: {
+        justifyContent: 'center',
+        alignItems: 'center',
+        flex: 1,
+      },
+      progressText: {
+        paddingTop: 50,
+        fontSize: 50,
+        color: "#fff"
+      },
+      button: {
+        padding: 20
+      },
+      disabledButtonText: {
+        color: '#eee'
+      },
+      buttonText: {
+        fontSize: 20,
+        color: "#fff"
+      },
+      activeButtonText: {
+        fontSize: 20,
+        color: "#B81F00"
+      }
+  
+    });
+  
+
+
